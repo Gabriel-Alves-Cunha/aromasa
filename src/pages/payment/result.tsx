1212e7b75d864e0f9dd07d331c4a38d84cc43d11
Gabriel Alves Cunha <gabriel925486@outlook.com>
2021-09-05 19:24:55 -0300

    Trying to deploy on vercel

--- src/pages/payment/result.tsx
+++ src/pages/payment/result.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 import { Loading, Header, getLayout } from "components";
 import { envVariables } from "storage/env";
 
-import error500_svg from "assets/errorVector/error_500.svg";
+import error500_svg from "src/assets/errorVector/error_500.svg";
 
 const contactEmail = envVariables.contactEmail;
 
