f6fa26784ad0be3eff751cbf7d95c48415aee100
Gabriel Alves Cunha <gabriel925486@outlook.com>
2021-10-02 09:37:24 -0300

    Refactored useCart

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -173,6 +173,13 @@ function CartProvider({ children }: CartProviderProps) {
 	);
 }
 
-const useCart = () => useContext(CartContext);
+const useCart = () => {
+	const context = useContext(CartContext);
+
+	if (context === undefined)
+		throw new Error("useCart must be used within a CartProvider");
+
+	return context;
+};
 
 export { useCart, CartProvider };
